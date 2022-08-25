89bb6a2b42895b0b9be541a3f7e98642c950ee71
Gilad Naaman <gilad@naaman.io>
2022-08-25 22:10:52 +0300

    Added option to disable progress bars

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,10 @@ struct PtsdArgs {
     /// Read commands from a file, line by line
     #[clap(long)]
     command_file: Option<PathBuf>,
+
+    /// Disable progress bars, only print failure report
+    #[clap(long, takes_value = false)]
+    disable_progress: bool,
 }
 
 const PROGRESS_TICK_FRAMES: &[&str] = &[
@@ -42,6 +46,29 @@ const PROGRESS_TICK_FRAMES: &[&str] = &[
     "( ●    )",
     "(●     )",
 ];
+
+fn spawn_task_process(
+    log_dir: &PathBuf,
+    task_name: &str,
+    shell: &str,
+    cmd: &str,
+) -> std::io::Result<tokio::process::Child> {
+    let mut stdout_file_path = log_dir.clone();
+    stdout_file_path.push(format!("{task_name}.stdout"));
+    let stdout = std::fs::File::create(stdout_file_path).unwrap();
+
+    let mut stderr_file_path = log_dir.clone();
+    stderr_file_path.push(format!("{task_name}.stderr"));
+    let stderr = std::fs::File::create(stderr_file_path).unwrap();
+
+    tokio::process::Command::new(shell)
+        .arg("-c")
+        .arg(cmd)
+        .stderr(stderr)
+        .stdout(stdout)
+        .stdin(Stdio::null())
+        .spawn()
+}
 
 #[tokio::main]
 async fn main() {
@@ -87,68 +114,74 @@ async fn main() {
     std::fs::create_dir_all(&log_dir).unwrap();
 
     // Calculate the character-width of the largest command index.
-    // This is used to align the log file names so they would be sortable by
+    // This is used  to align the log file names so they would be sortable by
     // command order.
     let width = (args.commands.len() as f32).log10() as usize + 1;
 
     eprintln!("Writing standard outputs to {log_dir:?}");
 
-    let mut tasks = Vec::new();
     let mut failed_tasks = Vec::new();
 
-    for (i, cmd) in args.commands.into_iter().enumerate() {
-        let pb = multi_progress_bar.add(ProgressBar::new_spinner());
-        pb.set_style(sty.clone());
-        pb.enable_steady_tick(Duration::from_millis(80));
-        pb.set_message(cmd.clone());
-        pb.set_prefix(format!("{i}"));
-
-        let done_sty = done_sty.clone();
-        let fail_sty = fail_sty.clone();
-
-        let mut stderr_file_path = log_dir.clone();
-        stderr_file_path.push(format!("{i:0width$}.stderr"));
-        let mut stdout_file_path = log_dir.clone();
-        stdout_file_path.push(format!("{i:0width$}.stdout"));
-
-        let stderr = std::fs::File::create(stderr_file_path).unwrap();
-        let stdout = std::fs::File::create(stdout_file_path).unwrap();
-
-        let proc = tokio::process::Command::new(&args.shell)
-            .arg("-c")
-            .arg(cmd)
-            .stderr(stderr)
-            .stdout(stdout)
-            .stdin(Stdio::null())
-            .spawn();
-
-        let mut proc = match proc {
-            Ok(proc) => proc,
-            Err(_) => {
-                pb.set_style(fail_sty);
-                pb.finish();
-                failed_tasks.push(i);
-                continue;
-            }
-        };
-
-        let handle = tokio::spawn(async move {
-            let res = proc.wait().await.unwrap();
-            if res.success() {
-                pb.set_style(done_sty);
+    // Convert the collected commands into async join_handles
+    let tasks: Vec<_> = args
+        .commands
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, cmd)| {
+            let pb = if args.disable_progress {
+                None
             } else {
-                pb.set_style(fail_sty);
-            }
-            pb.finish();
-            res.success()
-        });
-        tasks.push(handle);
-    }
+                let pb = multi_progress_bar.add(ProgressBar::new_spinner());
+                pb.set_style(sty.clone());
+                pb.enable_steady_tick(Duration::from_millis(80));
+                pb.set_message(cmd.clone());
+                pb.set_prefix(format!("{i}"));
+                Some(pb)
+            };
+
+            let done_sty = done_sty.clone();
+            let fail_sty = fail_sty.clone();
+
+            let mut proc =
+                match spawn_task_process(&log_dir, &format!("{i:0width$}"), &args.shell, &cmd) {
+                    Ok(proc) => proc,
+                    Err(_) => {
+                        pb.map(|pb| {
+                            pb.set_style(fail_sty);
+                            pb.finish();
+                        });
+                        failed_tasks.push(i);
+                        return None;
+                    }
+                };
+            let handle = tokio::spawn(async move {
+                let res = proc.wait().await.unwrap();
+                pb.map(|pb| {
+                    if res.success() {
+                        pb.set_style(done_sty);
+                    } else {
+                        pb.set_style(fail_sty);
+                    }
+                    pb.finish();
+                });
+                // Report the process exit code as task output
+                res.success()
+            });
+            Some((i, handle))
+        })
+        .collect();
 
     // Await the tasks and record failures
-    for (i, task) in tasks.into_iter().enumerate() {
-        if let Ok(false) | Err(_) = task.await {
-            failed_tasks.push(i);
+    for (task_index, handle) in tasks {
+        let result = handle.await;
+        match result {
+            Err(join_err) => {
+                eprintln!("Failed joining task {task_index}: {join_err:?}");
+            }
+            Ok(false) => {
+                failed_tasks.push(task_index);
+            }
+            _ => {}
         }
     }
 
